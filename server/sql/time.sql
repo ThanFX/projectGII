bd8aecd7eaf0cb97cff98e9e5a2154b777678b65
ThanFX <ZMikhael@gmail.com>
2016-06-14 23:54:28 +0600

    В скрипты дампа добавлено архивирование

---
+++ server/sql/time.sql
@@ -0,0 +1,17 @@
+﻿SELECT real_time, world_time, id, time_speed
+  FROM public."time";
+
+/* Количество секунд мира на момент старта новой разработки */
+UPDATE time SET world_time = 439555701 WHERE id = 1;
+
+SELECT * FROM config;
+SELECT * FROM persons;
+SELECT * FROM person_health_characteristic;
+
+INSERT INTO public.person_health_characteristic(
+            person_id, health, fatigue, hunger, thirst, somnolency)
+    VALUES (1, 100.0, 0.0, 0.0, 0.0, 0.0);
+
+UPDATE public.config SET 
+value = '{"State": 10, "HTS": 10, "ED": 10}'
+WHERE id = 'check_periods';
