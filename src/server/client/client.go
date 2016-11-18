d9a38c57e8a05198e92002a30f22262104d75591
ThanFX <ZMikhael@gmail.com>
2016-11-19 01:50:07 +0700

    Websocket server

--- src/server/client/client.go
+++ src/server/client/client.go
@@ -1,10 +1,14 @@
 package client
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"server/conf"
 	"text/template"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 var (
@@ -23,13 +27,32 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	homeTemplate.Execute(w, r.Host)
+	err := homeTemplate.Execute(w, r.Host)
+	if err != nil {
+		log.Fatal(w, "Template error: ", err)
+		return
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		return
+	}
+	fmt.Println("Соединение установлено!")
 }
 
 func ClientStart() {
 	go getTime()
 
+	r := mux.NewRouter()
 	http.HandleFunc("/", homeHandler)
+	r.HandleFunc("/ws", wsHandler)
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	err := http.ListenAndServe(conf.ADDR, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
